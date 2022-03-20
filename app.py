4844918230d95093948cd4af94143b6e1d1bff7f
Apurba Debnath <apurba871@gmail.com>
2022-03-20 12:51:51 +0530

    Created Voter model

--- app.py
+++ app.py
@@ -5,6 +5,17 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/election'
 db = SQLAlchemy(app)
 
+class Voter(db.Model):
+    voter_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    voter_cin = db.Column(db.String(16), unique=True, nullable=False)
+    voter_name = db.Column(db.String(30), nullable=False)
+    dept_code = db.Column(db.String(5), nullable=False)
+
+    def __repr__(self):
+        return f"Voter('{self.voter_id}', '{self.voter_name}')"
+
+
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World2!</p>"
