c0c42a31a3d8bcd7bb50634bb53d7bdcba8d9b18
Apurba Debnath <apurba871@gmail.com>
2022-03-27 17:19:08 +0530

    size fixes

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ class Voter(db.Model):
     voter_id = db.Column(db.Integer, primary_key=True, nullable=False)
     voter_cin = db.Column(db.String(16), unique=True, nullable=False)
     voter_name = db.Column(db.String(30), nullable=False)
-    dept_code = db.Column(db.String(5), db.ForeignKey('department.dept_code') ,nullable=False)
+    dept_code = db.Column(db.String(6), db.ForeignKey('department.dept_code') ,nullable=False)
 
     def __repr__(self):
         return f"Voter('{self.voter_id}', '{self.voter_name}')"
