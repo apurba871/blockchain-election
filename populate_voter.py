ce3c4125ab3b097071af1400cb39a4e4ab7b432f
Apurba Debnath <apurba871@gmail.com>
2022-03-27 16:42:23 +0530

    added students of BAGG dept into voter table

--- populate_voter.py
+++ populate_voter.py
@@ -1,14 +1,32 @@
-#trying to add all students of BAGG into voter table
+import mysql.connector
 
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="election"
+)
+
+my_cursor = mydb.cursor()
+voter_id = 0
+
+#trying to add all students of BAGG into voter table
 with open('BAGG.txt', 'r') as bagg:
     for line in bagg:
+        voter_id += 1
+
         record = line[1:-2].split(', ')
         name = record[0][record[0].find(':') + 1 : ]
         roll = record[1][record[1].find(':') + 1 : ]
         current_year = record[2][record[2].find(':') + 1 : ]
         registration_number = record[3][record[3].find(':') + 1 : ]
-        department = record[4][record[4].find(':') + 1 : ]
-        print(name, roll, current_year, registration_number, department)
+        department = record[4][record[4].find(':') + 1 : ][2:-1]
+        # print(name, roll, current_year, registration_number, department)
+        # print(department)
+
+        my_cursor.execute("INSERT INTO voter (voter_id, voter_cin, voter_name, dept_code) VALUES (%s, %s, %s, %s)", (voter_id, roll, name, department))
+mydb.commit()
+print("Success")
 
 # print(file_content[0].split(', ')[0][1:].index(": ") + 1)
 # print(file_content[0].split(', ')[1])
