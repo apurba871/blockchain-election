176df9e8a053fe911794b0e6c04e000a48921f22
Apurba Debnath <apurba871@gmail.com>
2022-03-27 17:18:48 +0530

    added students of all depts to voter table

--- populate_voter.py
+++ populate_voter.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import os
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -10,23 +11,31 @@ mydb = mysql.connector.connect(
 my_cursor = mydb.cursor()
 voter_id = 0
 
-#trying to add all students of BAGG into voter table
-with open('BAGG.txt', 'r') as bagg:
-    for line in bagg:
-        voter_id += 1
+file_list = ['BAGG.txt', 'BMBT.txt', 'BNGA.txt',
+             'CEMA.txt', 'CMSA.txt', 'ECOA.txt',
+             'ENGA.txt', 'MCBA.txt', 'MCVA.txt',
+             'MMFI.txt', 'MTMA.txt', 'PHSA.txt',
+             'PLSA.txt', 'SOCA.txt', 'STSA.txt']
 
-        record = line[1:-2].split(', ')
-        name = record[0][record[0].find(':') + 1 : ]
-        roll = record[1][record[1].find(':') + 1 : ]
-        current_year = record[2][record[2].find(':') + 1 : ]
-        registration_number = record[3][record[3].find(':') + 1 : ]
-        department = record[4][record[4].find(':') + 1 : ][2:-1]
-        # print(name, roll, current_year, registration_number, department)
-        # print(department)
+abs_path = os.path.dirname(os.path.abspath(__file__))
+for file in file_list:
+    current_file = abs_path + '/student_data/' + file
+    with open(current_file, 'r') as f:
+        for line in f:
+            voter_id += 1
 
-        my_cursor.execute("INSERT INTO voter (voter_id, voter_cin, voter_name, dept_code) VALUES (%s, %s, %s, %s)", (voter_id, roll, name, department))
+            record = line[1:-2].split(', ')
+            name = record[0][record[0].find(':') + 1 : ]
+            roll = record[1][record[1].find(':') + 1 : ]
+            current_year = record[2][record[2].find(':') + 1 : ]
+            registration_number = record[3][record[3].find(':') + 1 : ]
+            department = record[4][record[4].find(':') + 1 : ][2:-1]
+            # print(name, roll, current_year, registration_number, department)
+            # print(department)
+
+            my_cursor.execute("INSERT INTO voter (voter_id, voter_cin, voter_name, dept_code) VALUES (%s, %s, %s, %s)", (voter_id, roll, name, department))
 mydb.commit()
-print("Success")
+print("Success, total voters: ", voter_id)
 
 # print(file_content[0].split(', ')[0][1:].index(": ") + 1)
 # print(file_content[0].split(', ')[1])
