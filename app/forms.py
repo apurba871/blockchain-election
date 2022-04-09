efdbac4f0f85a9490a805b02191cfd3ae9aa0485
Apurba Debnath <apurba871@gmail.com>
2022-04-09 13:19:16 +0530

    Added optionality to update user profile

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import Voter
@@ -29,3 +31,24 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    cin = StringField('CIN', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired(), Length(min=3, max=255)])
+    # username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    dept = StringField('Dept Code', validators=[DataRequired(), Length(min=4, max=4)])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_cin(self, cin):
+        if cin.data != current_user.cin:
+            user = Voter.query.filter_by(cin=cin.data).first()
+            if user:
+                raise ValidationError('That CIN is already in use. Please choose a different one.')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = Voter.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That Email is taken. Please choose a different one.')
