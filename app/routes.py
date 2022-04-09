9594a08cd701d96cc6b10ba2230de78f541961fc
Apurba Debnath <apurba871@gmail.com>
2022-04-09 13:15:18 +0530

    Resized user profile pic to occupy less storage

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from app import app, db, bcrypt
 from app.models import Voter, Candidate, Election, Casted_Vote, Voter_List, Department
-from app.forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route("/")
@@ -42,11 +45,41 @@ def logout():
     logout_user()
     return redirect(url_for("index"))
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.imagefile = picture_file
+        current_user.cin = form.cin.data
+        current_user.name = form.name.data
+        current_user.email = form.email.data
+        current_user.dept = form.dept.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.cin.data = current_user.cin
+        form.name.data = current_user.name
+        form.email.data = current_user.email
+        form.dept.data = current_user.dept
     image_file = url_for('static', filename='profile_pics/' + current_user.imagefile)
-    return render_template("account.html", title="Account", image_file = image_file)
+    return render_template("account.html", title="Account", image_file=image_file, form=form)
 
 @app.route("/voter/<int:id>")
 def voter(id):
