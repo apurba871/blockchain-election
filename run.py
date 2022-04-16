e32f8b5efb86fe62ee5dc40f6b3e7f6ee9f581de
Apurba Debnath <apurba871@gmail.com>
2022-04-16 21:21:40 +0530

    Updating election state while starting the app

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import app
+from app import election_util
+import app.election_util as election_util
 
 if __name__ == '__main__':
+    election_util.update_election_state()
     app.run(debug=True)
 
 #Flask stuffs:
