3ae184c175cb4e9da060f28e09024e36f45258a0
Apurba Debnath <apurba871@gmail.com>
2022-04-16 21:22:58 +0530

    Storing password in encrypted format for all users

--- populate_voter.py
+++ populate_voter.py
@@ -2,6 +2,7 @@
 import sqlite3
 import json
 import os
+from app import bcrypt
 
 # mydb = mysql.connector.connect(
 #     host="localhost",
@@ -50,8 +51,54 @@ for file in file_list:
 mydb.commit()
 print("Success, total voters: ", voter_id)
 '''
+def populate_table():
+    file_list = ['BAGG.txt', 'BMBT.txt', 'BNGA.txt',
+                'CEMA.txt', 'CMSA.txt', 'ECOA.txt',
+                'ENGA.txt', 'MCBA.txt', 'MCVA.txt',
+                'MMFI.txt', 'MTMA.txt', 'PHSA.txt',
+                'PLSA.txt', 'SOCA.txt', 'STSA.txt',
+                'BOBA.txt', 'CMEA.txt', 'CMSL.txt',
+                'COMA.txt', 'EDUC.txt', 'HISA.txt',
+                'MCMF.txt', 'MCMM.txt', 'MCMS.txt',
+                'MMCB.txt', 'MPHY.txt']
+    try:
+        sqliteConnection = sqlite3.connect('app/site.db')
+        cursor = sqliteConnection.cursor()
+        print("Successfully Connected to SQLite")
 
-file_list = ['BAGG.txt', 'BMBT.txt', 'BNGA.txt',
+        abs_path = os.path.dirname(os.path.abspath(__file__))
+        for file in file_list:
+            current_file = abs_path + '/student_data/' + file
+            # print(current_file)
+            with open(current_file, 'r') as f:
+                for line in f:
+                    record = json.loads(line)
+                    name = record["Name"]
+                    cin = record["Roll"]
+                    email = cin + '@email.com'
+                    # current_year = record["Current Year"]
+                    # registration_number = record["Registration Number"]
+                    dept = record["Department"]
+                    imagefile = 'default.jpg'
+                    # password = 'password'
+                    password = bcrypt.generate_password_hash('password').decode('utf-8')
+                    join_year = 2016
+                    is_admin = False
+                    item = [cin, name, email, dept, imagefile, password, join_year, is_admin]
+                    # print(item)
+                    cursor.execute("INSERT INTO voter (cin, name, email, dept, imagefile, password, join_year, is_admin) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", item)
+        sqliteConnection.commit()
+        print("Successfully populated database!")
+        cursor.close()
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print("The SQLite connection is closed")
+
+def update_password():
+    file_list = ['BAGG.txt', 'BMBT.txt', 'BNGA.txt',
              'CEMA.txt', 'CMSA.txt', 'ECOA.txt',
              'ENGA.txt', 'MCBA.txt', 'MCVA.txt',
              'MMFI.txt', 'MTMA.txt', 'PHSA.txt',
@@ -60,41 +107,45 @@ file_list = ['BAGG.txt', 'BMBT.txt', 'BNGA.txt',
              'COMA.txt', 'EDUC.txt', 'HISA.txt',
              'MCMF.txt', 'MCMM.txt', 'MCMS.txt',
              'MMCB.txt', 'MPHY.txt']
-try:
-    sqliteConnection = sqlite3.connect('app/site.db')
-    cursor = sqliteConnection.cursor()
-    print("Successfully Connected to SQLite")
+    try:
+        sqliteConnection = sqlite3.connect('app/site.db')
+        cursor = sqliteConnection.cursor()
+        print("Successfully Connected to SQLite")
 
-    abs_path = os.path.dirname(os.path.abspath(__file__))
-    for file in file_list:
-        current_file = abs_path + '/student_data/' + file
-        # print(current_file)
-        with open(current_file, 'r') as f:
-            for line in f:
-                record = json.loads(line)
-                name = record["Name"]
-                cin = record["Roll"]
-                email = cin + '@email.com'
-                # current_year = record["Current Year"]
-                # registration_number = record["Registration Number"]
-                dept = record["Department"]
-                imagefile = 'default.jpg'
-                password = 'password'
-                join_year = 2016
-                is_admin = False
-                item = [cin, name, email, dept, imagefile, password, join_year, is_admin]
-                # print(item)
-                cursor.execute("INSERT INTO voter (cin, name, email, dept, imagefile, password, join_year, is_admin) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", item)
-    sqliteConnection.commit()
-    print("Successfully populated database!")
-    cursor.close()
-except sqlite3.Error as error:
-    print("Error while connecting to sqlite", error)
-finally:
-    if sqliteConnection:
-        sqliteConnection.close()
-        print("The SQLite connection is closed")
+        abs_path = os.path.dirname(os.path.abspath(__file__))
 
+        for file in file_list:
+            current_file = abs_path + '/student_data/' + file
+            # print(current_file)
+            with open(current_file, 'r') as f:
+                for line in f:
+                    record = json.loads(line)
+                    # name = record["Name"]
+                    cin = record["Roll"]
+                    # email = cin + '@email.com'
+                    # current_year = record["Current Year"]
+                    # registration_number = record["Registration Number"]
+                    # dept = record["Department"]
+                    # imagefile = 'default.jpg'
+                    # password = 'password'
+                    password = bcrypt.generate_password_hash('password').decode('utf-8')
+                    # join_year = 2016
+                    # is_admin = False
+                    item = [password, cin]
+                    # print(item)
+                    cursor.execute("UPDATE voter SET password=? WHERE cin=?", item)
+            print(file,"parsed and records updated!")
+        sqliteConnection.commit()
+        print("Successfully updated database!")
+        cursor.close()
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print("The SQLite connection is closed")
+
+update_password()
 
 # print(file_content[0].split(', ')[0][1:].index(": ") + 1)
 # print(file_content[0].split(', ')[1])
