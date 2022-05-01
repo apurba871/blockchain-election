265ad9c503fdc1ae40b9f90b32c057ce63216fc2
Apurba Debnath <apurba871@gmail.com>
2022-05-01 22:32:48 +0530

    wrote code for flask-mail

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail, Message
+# import flask_mail
 
 # from flask_wtf import FlaskForm
 # from wtforms import StringField, SubmitField
@@ -12,6 +14,22 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 app.config['SECRET_KEY'] = '6ed92a393d113812533944e21b26b07a'
+
+# flask-mail stuffs...
+app.config['TESTING'] = False
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+# app.config['MAIL_DEBUG'] = True
+app.config['MAIL_USERNAME'] = 'blockchainvoting7@gmail.com'
+app.config['MAIL_PASSWORD'] = 'sjzlmyisvxlopcvt'
+app.config['MAIL_DEFAULT_SENDER'] = 'blockchainvoting7@gmail.com'
+app.config['MAIL_MAX_EMAILS'] = None
+# app.config['MAIL_SUPPRESS_SEND'] = False
+app.config['MAIL_ASCII_ATTACHMENTS'] = False
+
+mail = Mail(app)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
