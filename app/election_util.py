31547a4b4cd329dc8d1ce373cd60c0bf4935eabf
tigerjoy <ranajoy123saha@gmail.com>
2022-05-05 00:48:40 +0530

    Added utility functions for saving and combining votes

--- app/election_util.py
+++ app/election_util.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+import requests
 
 def update_election_state():
     from app import db
@@ -92,8 +92,76 @@ def transform_vote(election_id, candidate_id):
     max_dig = len(str(voter_count))
     return 10 ** (max_dig * (candidate_id - 1))
 
-def encrypt_vote(election_id):
-    pass
+def encrypt_vote(election_id, t_vote):
+    import app.paillier_utils as paillier_utils
+    from app.models import Election
+    election_record = Election.getElectionRecord(election_id)
+    (cipher_text, exp) = paillier_utils.encrypt_value(election_record.public_key, t_vote)
+    return cipher_text, exp 
+
+def create_shares(election_id, candidate_id):
+    from secretsharing import SecretSharer
+    t_vote = transform_vote(election_id, candidate_id)
+    cipher_text, exp = encrypt_vote(election_id, t_vote)
+    shares = SecretSharer.split_secret(cipher_text, 2, 3)
+    return shares, exp
+
+def combine_shares(shares):
+    from secretsharing import SecretSharer
+    return SecretSharer.recover_secret(shares)
+
+def save_shares(election_id, candidate_id):
+    urls = ["http://127.0.0.1:5100", "http://127.0.0.1:5200", "http://127.0.0.1:5300"]
+    end_point = "/storeVote"
+    shares, exp = create_shares(election_id, candidate_id)
+    results = []
+    for url, share in zip(urls, shares):
+        r = requests.post(url+end_point, 
+                    params={'election_id': election_id, 'exponent': exp, 'share':share})
+        results.append(r.json())
+    print(results)
+
+# {   
+#     "success":True, 
+#     "election_id": election_id, 
+#     "exponent":exponent, 
+#     "shares":[
+#           {'id': s.share_id, 'share': s.share},
+#           {'id': s.share_id, 'share': s.share},
+#           .... 
+#       ]
+# }
+
+def parse_shares(results):
+    shares1 = [item['share'] for item in sorted(results[0]["shares"], key=lambda d: d['id'])]
+    shares2 = [item['share'] for item in sorted(results[1]["shares"], key=lambda d: d['id'])] 
+    shares3 = [item['share'] for item in sorted(results[2]["shares"], key=lambda d: d['id'])]
+    encrypted_votes = [combine_shares(list(share)) for share in zip(shares1, shares2, shares3)]
+    return encrypted_votes
+
+def get_all_combined_votes(election_id):
+    urls = ["http://127.0.0.1:5100", "http://127.0.0.1:5200", "http://127.0.0.1:5300"]
+    end_point = "/getVote"
+    results = []
+    for url in urls:
+        r = requests.post(url+end_point, 
+                        params={'election_id': election_id})
+        results.append(r.json())
+    return parse_shares(results)
+
+# Where votes is a list of encrypted votes
+def count_votes(votes, exp, election_id):
+    import app.paillier_utils as paillier_utils
+    from app.models import Election
+    election_record = Election.getElectionRecord(election_id)
+    total = None
+    for vote in votes:
+        pailier_obj = paillier_utils.convert_to_paillier_obj(vote, exp, election_record.public_key)
+        if total is None:
+            total = pailier_obj
+        else:
+            total = total + pailier_obj
+    return paillier_utils.paillier_obj_to_tuple(total)
 
 if __name__ == "__main__":
     print(get_new_election_id())
