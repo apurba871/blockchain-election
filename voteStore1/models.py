41274ecd1f59c9e339cc4f577af38cae4daba403
tigerjoy <ranajoy123saha@gmail.com>
2022-05-04 21:57:07 +0530

    voteStore1 initial commit

---
+++ voteStore1/models.py
@@ -0,0 +1,30 @@
+from voteStore1 import db
+
+class Access(db.Model):
+  election_id = db.Column(db.String(5),primary_key=True, nullable=False)
+  has_access = db.Column(db.Boolean, default=False, nullable=False)
+  
+  @classmethod
+  def is_present(cls, election_id):
+    return True if Access.query.filter_by(election_id=election_id).first() else False
+
+  @classmethod
+  def get_has_access(cls, election_id):
+    access_obj = Access.query.filter_by(election_id=election_id).first()
+    if access_obj is not None:
+      return access_obj.has_access
+    else:
+      return False
+  
+  @classmethod
+  def getRecord(cls, election_id):
+    return Access.query.filter_by(election_id=election_id).first()
+
+class Share(db.Model):
+  election_id = db.Column(db.String(5),  db.ForeignKey('access.election_id'), primary_key=True, nullable=False)
+  part_no = db.Column(db.Integer, nullable=False, primary_key=True)
+  share = db.Column(db.Text, nullable=False)
+
+  @classmethod
+  def getShares(cls, election_id):
+    return Share.query.filter_by(election_id=election_id).first()
