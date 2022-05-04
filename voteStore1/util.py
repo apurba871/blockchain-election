b31f22b2551f78e7b77418f7bc471ca3775fb245
tigerjoy <ranajoy123saha@gmail.com>
2022-05-04 22:56:57 +0530

    Updated voteStore1 to store the value of exponent

--- voteStore1/util.py
+++ voteStore1/util.py
@@ -1,12 +1,12 @@
 from voteStore1 import db
 from voteStore1.models import Share, Access
 
-def store_vote_helper(election_id, share):
+def store_vote_helper(election_id, exponent, share):
   if not Access.is_present(election_id=election_id):
     access_obj = Access(election_id=election_id)
     db.session.add(access_obj)
     db.session.commit()
-  stored_vote = Share(election_id=election_id, share=share)
+  stored_vote = Share(election_id=election_id, exponent=exponent, share=share)
   db.session.add(stored_vote)
   db.session.commit()
 
