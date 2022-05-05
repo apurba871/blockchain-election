cdfb9b996d49edd28b16a4e7d0c5bada00f3d432
tigerjoy <ranajoy123saha@gmail.com>
2022-05-05 07:24:47 +0530

    Modified

--- app/constants.py
+++ app/constants.py
@@ -1,5 +1,5 @@
 provider_address = "HTTP://127.0.0.1:7545"
 chain_id = 1337
-my_address = "0x5cB4BA24fb4Ca0f7D2A88942c9bc01D9c5a3C128"
-private_key = "d9c74a6a0942fdea70100b77fb73c8488e15c396b4489d5e1c6a7e8a75773b18"
-contractAddress = '0xa08E59419A2998137aa8b15b64a94aE27E582048'
+my_address = "0x7C782AD534866332bd9c1543168bAcB822c95459"
+private_key = "cc3492cfd1cc77e498108d77e63e69d5cf88422be1d07910182c107a6cff732a"
+contractAddress = '0x9b2D5FD8EaC0A9d5750F741Fe573f67E0487012A'
